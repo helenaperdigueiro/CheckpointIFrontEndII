50e0cfe622512fe3138bc1364e87e536028be9c4
helenaperdigueiro <helena.perdigueiro@gmail.com>
2021-09-01 15:20:43 -0300

    2a versao

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
-let title = "";
-let urlImg = "";
-let descriptionImg = "";
-let buttonSubmit = document.getElementById("submit");
+document.getElementById("submit").addEventListener("click", function () {
+    let title = document.getElementById("title").value;
+    let picture = document.getElementById("picture").value;
+    let description = document.getElementById("description").value;
+    let card = document.getElementById("card");
 
-buttonSubmit.onclick = function createCard() {
-    title = document.getElementById("title").value;
-    urlImg = document.getElementById("picture").value;
-    descriptionImg = document.getElementById("description").value;
+    console.log(title);
+    console.log(picture);
+    console.log(description);
 
-    let cardSection = document.getElementById("card");
+    document.getElementById("title").focus();
+})
 
-    let teste = document.createElement("h1");
-
-    teste.innerHTML = title;
-    // cardSection.innerHTML += `<div><img src="${urlImg}"></div>`;
-    // cardSection.innerHTML += `<div><h5>${descriptionImg}</h5></div>`;
-
-    cardSection.appendChild(teste);
-    // teste.appendChild(title);
-
-    document.querySelector("body").appendChild(teste);
-
-}
+até mais tarde, povo!
+Helena, lembre de dar o push para a gente dar o pull e tentar descobrir o erro.
