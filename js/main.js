7742543da5d5e84cdbbcee42dab7b1e72fe4510a
AillaF <aillaf@gmail.com>
2021-09-08 14:12:11 -0300

    refatoração do js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
+let title = document.getElementById("title");
+let picture = document.getElementById("picture");
+let description = document.getElementById("description");
+let cards = document.getElementById("card");
+let contact = document.getElementById("contact");
+let team = document.getElementById("team");
+
 document.querySelector("form").onsubmit = function () { return false };
 
 document.getElementById("submit").addEventListener("click", function () {
-  if ((document.getElementById("title").value == "") || (document.getElementById("picture").value == "") || (document.getElementById("description").value == "")) {
-    alert("Preencha os 3 campos!");
-  } else {
-    let title = document.getElementById("title").value;
-    let picture = document.getElementById("picture").value;
-    let description = document.getElementById("description").value;
-    let cards = document.getElementById("card");
-    let card = document.createElement("div");
+  if ((title.value == "") || (picture.value == "") || (description.value == "")) {
+    document.getElementById("title").focus();
 
+    document.querySelectorAll("input").forEach((item) => item.value = "");
+
+    alert("Por favor, preencha os 3 campos!");
+  } else {
+    let card = document.createElement("div");
     card.setAttribute("class", "card");
     cards.appendChild(card);
 
-    card.innerHTML += `<h2 class="cardTitle">${title}</h2>`;
-    card.innerHTML += `<img src="${picture}" class="cardPicture">`;
-    card.innerHTML += `<h3 class="cardDescription">${description}</h3>`;
+    card.innerHTML += `<h2 class="cardTitle">${title.value}</h2>`;
+    card.innerHTML += `<img src="${picture.value}" class="cardPicture">`;
+    card.innerHTML += `<h3 class="cardDescription">${description.value}</h3>`;
 
     let btnDelete = document.createElement("img");
     btnDelete.setAttribute("src", "https://image.flaticon.com/icons/png/512/463/463612.png");
@@ -30,17 +36,17 @@ document.getElementById("submit").addEventListener("click", function () {
 })
 
 function openContact() {
-    document.getElementById("myForm").style.display = "block";
+    contact.style.display = "block";
   }
   
   function closeContact() {
-    document.getElementById("myForm").style.display = "none";
+    contact.style.display = "none";
   }
 
   function openTeam() {
-    document.getElementById("myForm2").style.display = "block";
+    team.style.display = "block";
   }
   
   function closeTeam() {
-    document.getElementById("myForm2").style.display = "none";
+    team.style.display = "none";
   }
